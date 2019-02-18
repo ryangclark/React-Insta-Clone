999b7732f64c525b8c440484dc643f0c071c136d
Clark <rclark010@gmail.com>
2019-02-18 16:32:13 -0700

    Added FontAwesome regular, getting SearchBar up.

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -3,10 +3,14 @@ import './SearchBar.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import CameraLogo from '../../assets/camera-logo.svg';
+import IGLogo from '../../assets/iglogo.png';
+
 const SearchBar = () => {
     return (
         <header className="search-bar">
-          <h1>Instagram</h1>
+            <img alt="camera logo" class="camera-logo" src={CameraLogo} />
+            <img alt="Instagram Logo" class="instagram-logo" src={IGLogo} />
 
           <FontAwesomeIcon icon="compass" />
           <FontAwesomeIcon icon="heart" />
