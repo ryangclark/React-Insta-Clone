2123d6289f404d3192d2c378768a6330d0964f57
Clark <rclark010@gmail.com>
2019-02-18 15:07:44 -0700

    Setting up the basics.

---
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CommentSection.css';
+
+const CommentSection = () => {
+    return (
+        <div className="comment-section">CommentSection</div>
+    );
+}
+
+export default CommentSection;
---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './PostContainer.css';
+
+const PostContainer = () => {
+    return (
+        <div className="post-container">PostContainer</div>
+    );
+}
+
+export default PostContainer;
