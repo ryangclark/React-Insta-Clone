16d4a19184c4e55fb63ed9de57e1d0747cca6965
Clark <rclark010@gmail.com>
2019-02-18 16:07:22 -0700

    Bringing in FontAwesome

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCompass, faHeart, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
 import dummyData from './dummy-data.js';
 
 import PostContainer from './components/PostContainer/PostContainer';
@@ -10,20 +14,22 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      dummyData: dummyData
+      posts: dummyData
     }
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Instagram</h1>
-        </header>
         <SearchBar />
-        <PostContainer />
+        {this.state.posts.map(post => <PostContainer post={post}/>)}
       </div>
     );
   }
 }
+
+// FontAwesome stuff
+library.add(faCompass);
+library.add(faHeart);
+library.add(faUserCircle);
 
 export default App;
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './SearchBar.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const SearchBar = () => {
     return (
-        <div className="search-bar">SearchBar</div>
+        <header className="search-bar">
+          <h1>Instagram</h1>
+
+          <FontAwesomeIcon icon="compass" />
+          <FontAwesomeIcon icon="heart" />
+          <FontAwesomeIcon icon="user-circle" />
+        </header>
     );
 }
 
